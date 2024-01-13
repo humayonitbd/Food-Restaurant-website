ca49183557661fb5a1fc6bb82c34eab0d6e00ae1
humayonitbd <humayonforid44@gmail.com>
2024-01-13 20:52:48 +0600

    error page

--- src/Main/SharedPage/ErrorPage/ErrorPage.jsx
+++ src/Main/SharedPage/ErrorPage/ErrorPage.jsx
@@ -1,28 +1,26 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useContext } from "react";
-import { Link, useNavigate, useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 // import errorImage from "../../../src/assets/baneer-img/ErrorImage.png";
-import { AuthContext } from "../../Context/AuthProvider";
+// import { AuthContext } from "../../Context/AuthProvider";
 
 const ErrorPage = () => {
   const error = useRouteError();
-  const navigete = useNavigate();
-  const { logOut } = useContext(AuthContext);
-  const handlerSignOut = () => {
-    logOut()
-      .then(() => {
-        alert("LogOut successfull!!");
-        navigete("/");
-      })
-      .catch((err) => console.error(err));
-  };
+  // const navigete = useNavigate();
+  // const { logOut } = useContext(AuthContext);
+  // const handlerSignOut = () => {
+  //   logOut()
+  //     .then(() => {
+  //       alert("LogOut successfull!!");
+  //       navigete("/");
+  //     })
+  //     .catch((err) => console.error(err));
+  // };
   return (
     <section className="flex items-center h-screen p-16 bg-gray-100 text-gray-900">
       <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
-        <div>
-          {/* <img className="w-28" src={errorImage} alt="" /> */}
-        </div>
+        <div>{/* <img className="w-28" src={errorImage} alt="" /> */}</div>
         <div className="max-w-md text-center">
           <h2 className="mb-8 font-extrabold text-9xl text-gray-500">
             <span className="sr-only">Error</span>
@@ -36,10 +34,8 @@ const ErrorPage = () => {
             Sorry, we could not find this page.
           </p>
           <p>{error.statusText || error.message}</p>
-          <Link>
-            <button className="btn bg-orange-500" onClick={handlerSignOut}>
-              log-out
-            </button>
+          <Link to="/">
+            <button className="btn bg-[#F01543] px-10">Back to home</button>
           </Link>
         </div>
       </div>
